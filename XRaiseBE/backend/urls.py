a08299965e10b6193915102e27784c5866974828
Aly Ahmady <better.aly.ahmady@gmail.com>
2025-12-11 14:16:42 -0500

    fix(backend): add missing static files' paths to URL patterns

--- XRaiseBE/backend/urls.py
+++ XRaiseBE/backend/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -6,6 +8,7 @@ from backend.views import HealthCheckView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/health/", HealthCheckView.as_view(), name="health-check"),
