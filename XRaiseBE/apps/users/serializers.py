d8888e8bfc861de54c6d6e2da9696f499b1cf83e
Aly Ahmady <better.aly.ahmady@gmail.com>
2025-12-11 14:33:58 -0500

    chore(users): replace `get_user_model` with actual `users.User` model

--- XRaiseBE/apps/users/serializers.py
+++ XRaiseBE/apps/users/serializers.py
@@ -1,8 +1,7 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-User = get_user_model()
+from apps.users.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
