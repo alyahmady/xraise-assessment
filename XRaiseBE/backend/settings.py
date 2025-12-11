cc0f0e0314db56114a109f54eaab98ee63523a21
Aly Ahmady <better.aly.ahmady@gmail.com>
2025-12-11 14:20:12 -0500

    chore(backend): remove unused `rest_framework.authtoken` app

--- XRaiseBE/backend/settings.py
+++ XRaiseBE/backend/settings.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.authtoken",
     "corsheaders",
     "drf_spectacular",
     "apps.users",
